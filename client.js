fc43c920d4228ad41a2e4b80d38f3eec303190ab
Umar Hadi Siswanto <hi@umarhadi.xyz>
2021-04-10 22:22:26 +0800

    everything is works!

--- client.js
+++ client.js
@@ -1,34 +1,34 @@
 const socket = io('http://localhost:3000')
-const kontainerPesan = document.getElementById('kontainer-pesan')
-const formPesan = document.getElementById('kontainer-kirim')
-const inputPesan = document.getElementById('input-pesan')
+const messageContainer = document.getElementById('kontainer-pesan')
+const messageForm = document.getElementById('kontainer-kirim')
+const messageInput = document.getElementById('input-pesan')
 
-const name = prompt('Siapa kamu?')
+const name = prompt('What is your name?')
 appendMessage('Kamu bergabung')
 socket.emit('user-baru', name)
 
 socket.on('isi-pesan', data => {
-    appendMessage(`${data.name}: ${data.message}`)
+  appendMessage(`${data.name}: ${data.message}`)
 })
 
 socket.on('user-bergabung', name => {
-    appendMessage(`${name} bergabung ke chat`)
+  appendMessage(`${name} bergabung`)
 })
 
 socket.on('user-keluar', name => {
-    appendMessage(`${name} keluar dari chat`)
+  appendMessage(`${name} keluar`)
 })
 
 messageForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const message = messageInput.value
-    appendMessage(`Kamu: ${message}`)
-    socket.emit('kirim-pesan', message)
-    messageInput.value = ''
+  e.preventDefault()
+  const message = messageInput.value
+  appendMessage(`You: ${message}`)
+  socket.emit('kirim-isi-pesan', message)
+  messageInput.value = ''
 })
 
 function appendMessage(message) {
-    const messageElement = document.createElement('div')
-    messageElement.innerText = message
-    messageContainer.append(messageElement)
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
 }
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const io = require('socket.io')(3000)
 
 const users = {}
-// register user yang baru masuk ke server dan melakukan join
+
 io.on('connection', socket => {
   socket.on('user-baru', name => {
     users[socket.id] = name
-    socket.broadcast.emit('user-gabung', name)
+    socket.broadcast.emit('user-bergabung', name)
   })
-  socket.on('kirim-pesan', message => {
+  socket.on('kirim-isi-pesan', message => {
     socket.broadcast.emit('isi-pesan', { message: message, name: users[socket.id] })
   })
   socket.on('disconnect', () => {
